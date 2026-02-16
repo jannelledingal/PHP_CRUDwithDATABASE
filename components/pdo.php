af5916f8ce70b73b933669597c3685a66b2378c6
jannelledingal <j.dingal.560136@umindanao.edu.ph>
2026-02-17 00:19:47 +0800

    update

--- components/pdo.php
+++ components/pdo.php
@@ -1,8 +1,9 @@
 <?php
 $pdo = new PDO(
-  "mysql:host=localhost;dbname=usermnt;charset=utf8mb4", // Matches your screenshot
+  "mysql:host=localhost;dbname=usermnt;charset=utf8mb4", 
   "root",
   "",
   [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
 );
+
 ?>
